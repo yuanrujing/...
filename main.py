24d50a3220aa33fff91da713985da765ca50da98
yuanrujing <112389038+yuanrujing@users.noreply.github.com>
2022-08-29 18:42:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -161,8 +161,8 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "value": wind_dir,
                 "color": get_color()
             },
-            "love_day": {
-                "value": love_days,
+            "friendly_day": {
+                "value": friendly_days,
                 "color": get_color()
             },
             "note_en": {
